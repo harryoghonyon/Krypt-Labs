090d792ac59988a84124a00a1f8dc912be5e1f6c
Harry Oghonyon <harrytones9@gmail.com>
2023-02-28 01:14:58 +0100

    Work on mobile section and styling

---
+++ script.js
@@ -0,0 +1,15 @@
+// const heroImg = document.querySelector('.heroImg');
+
+// function rotate() {
+//   heroImg.style.transform = 'rotate(0deg)';
+//   heroImg.animate([
+//     { transform: 'rotate(0deg)' },
+//     { transform: 'rotate(360deg)' }
+//   ], {
+//     duration: 20000,
+//     iterations: Infinity,
+//     easing: 'linear'
+//   });
+// }
+
+// rotate();
